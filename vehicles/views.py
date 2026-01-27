016d34d6e2713b6f5358806118893eeb38305162
ghu0101exe <g.huy0102005@gmail.com>
2026-01-28 02:53:39 +0700

    Provide an API for displaying a list of vehicles on a map.

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Vehicle
 
-# Create your views here.
+def vehicle_list_api(request):
+    """
+    API trả về danh sách xe dạng JSON cho Frontend và Map 
+    """
+    vehicles = Vehicle.objects.all()
+    data = []
+    for v in vehicles:
+        data.append({
+            'id': v.id,
+            'name': v.name,
+            'license_plate': v.license_plate,
+            'lat': v.latitude,
+            'lng': v.longitude,
+            'price': float(v.price_per_hour),
+            'status': v.status,
+            'image_url': v.image.url if v.image else ''
+        })
+    return JsonResponse({'vehicles': data})
