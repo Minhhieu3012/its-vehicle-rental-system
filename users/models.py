0a82922c03aaed5de65790ee82f2c8e34e098a96
ghu0101exe <g.huy0102005@gmail.com>
2026-01-28 02:45:41 +0700

    Create model files to test the logic.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from vehicles.models import Vehicle
 
-# Create your models here.
+class Booking(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Chờ duyệt'),
+        ('CONFIRMED', 'Đã duyệt'),
+        ('COMPLETED', 'Hoàn thành'),
+        ('CANCELLED', 'Đã hủy'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    total_price = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking {self.id} - {self.user.username}"
--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Vehicle(models.Model):
+    # Định nghĩa các trạng thái xe 
+    STATUS_CHOICES = [
+        ('AVAILABLE', 'Sẵn sàng'),
+        ('BOOKED', 'Đã đặt'),
+        ('IN_USE', 'Đang đi'),
+        ('MAINTENANCE', 'Bảo trì'),
+    ]
+
+    name = models.CharField(max_length=100)
+    license_plate = models.CharField(max_length=20, unique=True) # Biển số
+    image = models.ImageField(upload_to='vehicles/', blank=True, null=True) # 
+    price_per_hour = models.DecimalField(max_digits=10, decimal_places=2) # Giá gốc
+    
+    # Tọa độ cho team Map (Leaflet) 
+    latitude = models.FloatField(default=10.8231) # Ví dụ: Tọa độ SG
+    longitude = models.FloatField(default=106.6297)
+    
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='AVAILABLE')
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.license_plate}"
