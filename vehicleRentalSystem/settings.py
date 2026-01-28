4ace0b9cc80f04ab8901013caa9546e82083ed7d
GiaBaoo <ngbao840@gmail.com>
2026-01-29 01:47:03 +0700

    add media_url media_root to upload image

--- vehicleRentalSystem/settings.py
+++ vehicleRentalSystem/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
