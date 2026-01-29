3acf4cd9126f6be7b54f8e79c6b32f230b3e0e86
GiaBaoo <ngbao840@gmail.com>
2026-01-30 00:10:19 +0700

    update models.py of bookings

--- bookings/models.py
+++ bookings/models.py
@@ -41,4 +41,4 @@ class Booking(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Booking #{self.id} - {self.user.username}"
+        return f"Booking #{self.id} - {self.customer.username}"
