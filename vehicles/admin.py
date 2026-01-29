211f5a52bb863de80a85b2669bca6ba737a4b362
GiaBaoo <ngbao840@gmail.com>
2026-01-30 00:09:27 +0700

    update admin.py of bookings, users, vehicles: admin page display data

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Vehicle
 # Register your models here.
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'license_plate',
+        'vehicle_type',
+        'status',
+        'price_per_day',
+        'latitude',
+        'longitude',
+        'created_at',
+    )
+
+    list_filter = ('status', 'vehicle_type')
+    search_fields = ('name', 'license_plate')
